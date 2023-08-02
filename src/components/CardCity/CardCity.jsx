ed7eee16e433592b00fb167180613f88441b4cb8
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-08-02 17:31:25 +0200

    Cambios en la funcionalidad de Like en Ciudades

--- src/components/CardCity/CardCity.jsx
+++ src/components/CardCity/CardCity.jsx
@@ -12,6 +12,9 @@ const CardCity = ({ city }) => {
 
   return (
     <section className="city-Info">
+      <div className="city_ToggleBtnFollowUser">
+        <ToggleBtnFollowCity cityToFollowId={city._id} />
+      </div>
       <button
         className="city-card-btn"
         onClick={() =>
@@ -30,20 +33,21 @@ const CardCity = ({ city }) => {
             {/*--- Este componente hace la media de las estrellas ---*/}
             <ReadOnlyCityRating city={city} />
             <p>({city?.ratings?.length} valoraciones)</p>
-            {/* {city.city} */}
           </div>
         </div>
         <div className="city-Info-toda_la_carta">
           <div className="city-Info-routeName-descriptionGeneral">
             <div>
               <h4 className="city-Info-name">{city.routeName}</h4>
-              <p className="city-Info-descriptionGeneral">{city.descriptionGeneral}</p>
+              <p className="city-Info-descriptionGeneral">
+                {city.descriptionGeneral}
+              </p>
             </div>
-            <div>
-              <div className="city_ToggleBtnFollowUser">
-                <ToggleBtnFollowCity cityToFollowId={city._id} />
-              </div>
-            </div>
+            {/* <div>
+                            <div className="mountainRoute_ToggleBtnFollowMontainRoute">
+                                <ToggleBtnFollowMountainRoute mountainRouteToFollowId={mountainRoute._id} />
+                            </div>
+                        </div> */}
           </div>
           <div className="city-Info-separar-info-general-routeState">
             <div className="city-Info-separar-info-general">
@@ -63,7 +67,8 @@ const CardCity = ({ city }) => {
               </p>
               <p className="">
                 {' '}
-                <FcDepartment className="city-FcDepartment" /> {city.routeLocation}
+                <FcDepartment className="city-FcDepartment" />{' '}
+                {city.routeLocation}
               </p>
             </div>
             <div className="city-Info-separar-routeState">
@@ -77,3 +82,86 @@ const CardCity = ({ city }) => {
 };
 
 export default CardCity;
+
+//Version Anterior
+// import './CardCity.css';
+
+// import { FcAddRow, FcClock, FcDepartment, FcEditImage } from 'react-icons/fc';
+// import { useNavigate } from 'react-router-dom';
+
+// import ReadOnlyCityRating from '../ratings/ReadOnlyCityRating/ReadOnlyCityRating';
+// import ToggleBtnFollowCity from '../ToggleBtnFollowCity/ToggleBtnFollowCity';
+
+// const CardCity = ({ city }) => {
+//   const navigate = useNavigate();
+//   const pathById = `/cityDetails`;
+
+//   return (
+//     <section className="city-Info">
+//       <button
+//         className="city-card-btn"
+//         onClick={() =>
+//           navigate(pathById, {
+//             state: { id: city._id },
+//           })
+//         }
+//       >
+//         <div className="city-Info-img-valoraciones">
+//           <img
+//             className="city-Info-img"
+//             src={city.image}
+//             alt={`city's ${city.routeName} pic`}
+//           />
+//           <div>
+//             {/*--- Este componente hace la media de las estrellas ---*/}
+//             <ReadOnlyCityRating city={city} />
+//             <p>({city?.ratings?.length} valoraciones)</p>
+//             {/* {city.city} */}
+//           </div>
+//         </div>
+//         <div className="city-Info-toda_la_carta">
+//           <div className="city-Info-routeName-descriptionGeneral">
+//             <div>
+//               <h4 className="city-Info-name">{city.routeName}</h4>
+//               <p className="city-Info-descriptionGeneral">{city.descriptionGeneral}</p>
+//             </div>
+//             <div>
+              
+//             </div>
+//           </div>
+//           <div className="city-Info-separar-info-general-routeState">
+//             <div className="city-Info-separar-info-general">
+//               <p className="city-Info-distance">
+//                 {' '}
+//                 <FcEditImage className="city-FcEditImage" />
+//                 {city.routeDistance} km
+//               </p>
+//               <p className="city-Info-routeDuration">
+//                 {' '}
+//                 <FcClock className="city-FcClock" />
+//                 {city.routeDuration} Horas
+//               </p>
+//               <p className="">
+//                 <FcAddRow className="city-FcAddRow" />
+//                 {city.difficulty}
+//               </p>
+//               <p className="">
+//                 {' '}
+//                 <FcDepartment className="city-FcDepartment" /> {city.routeLocation}
+//               </p>
+//             </div>
+//             <div className="city-Info-separar-routeState">
+//               <p className="">{city.routeState}</p>
+//             </div>
+            
+//           </div>
+//         </div>
+//         <div className="city_ToggleBtnFollowUser">
+//                 <ToggleBtnFollowCity cityToFollowId={city._id} />
+//         </div>
+//       </button>
+//     </section>
+//   );
+// };
+
+// export default CardCity;
--- src/components/ToggleBtnFollowCity/ToggleBtnFollowCity.jsx
+++ src/components/ToggleBtnFollowCity/ToggleBtnFollowCity.jsx
@@ -1,12 +1,12 @@
 import './ToggleBtnFollowCity.css';
 
 import { useEffect, useState } from 'react';
-import { AiOutlineHeart, AiTwotoneHeart } from 'react-icons/ai';
-
-import {
-  getCityFollowingStatus,
-  toggleInterestedCityToUser,
-} from '../../services/API_proyect/city.service';
+//import { AiOutlineHeart, AiTwotoneHeart } from 'react-icons/ai';
+import { RiUserFollowFill, RiUserUnfollowFill } from 'react-icons/ri';
+ import {
+   getCityFollowingStatus,
+   toggleInterestedCityToUser,
+ } from '../../services/API_proyect/city.service';
 
 const ToggleBtnFollowCity = ({ cityToFollowId }) => {
   const [isFavorite, setIsFavorite] = useState(false);
@@ -16,7 +16,7 @@ const ToggleBtnFollowCity = ({ cityToFollowId }) => {
       try {
         const isFav = await getCityFollowingStatus(cityToFollowId);
 
-        if (isFav?.data?.status === 'cityRoute is in following arr') {
+        if (isFav?.data?.status === "Route is in user's cityRoutesInterested array") {
           setIsFavorite(true);
         } else {
           setIsFavorite(false);
@@ -30,29 +30,107 @@ const ToggleBtnFollowCity = ({ cityToFollowId }) => {
 
   const handleAddToFavorites = async () => {
     try {
-      const response = await toggleInterestedCityToUser(cityToFollowId);
-      if (response?.data?.status === 'Success updating following -- Followers') {
+      const response = await toggleInterestedCityToUser(
+        cityToFollowId,
+      );
+      if (
+        response?.data ===
+          "City route added to user's cityRoutesInterested array" ||
+        response?.data ===
+          "City route removed from user's cityRoutesInterested array"
+      ) {
         setIsFavorite(!isFavorite);
       }
-
-      // setIsFavorite(!isFavorite);
     } catch (error) {
       console.error('Error while adding to favorites', error);
     }
   };
 
   return (
-    <button
-      onClick={handleAddToFavorites}
-      className={`ToggleButtonCity ${isFavorite ? 'favorite' : ''}`}
-    >
-      {isFavorite ? (
-        <AiTwotoneHeart size={25} className="favorite-icon" />
-      ) : (
-        <AiOutlineHeart size={25} className="favorite-icon" />
-      )}
-    </button>
+    <div>
+      <button
+        onClick={handleAddToFavorites}
+        className={`no-border-button ${isFavorite ? 'favorite' : ''}`}
+      >
+        {isFavorite ? (
+          <p>
+            {' '}
+            {/* Dejar de seguirla{' '} */}
+            <RiUserUnfollowFill size={25} className="favorite-icon-pendiente-seguir" />
+          </p>
+        ) : (
+          <p>
+            {' '}
+            {/* Sigue esta ruta de ciudad{' '} */}
+            <RiUserFollowFill size={25} className="favorite-icon-siguiendo" />{' '}
+          </p>
+        )}
+      </button>
+    </div>
   );
 };
 
+
+
 export default ToggleBtnFollowCity;
+
+
+//Version anterior y funcional
+// import './ToggleBtnFollowCity.css';
+
+// import { useEffect, useState } from 'react';
+// import { AiOutlineHeart, AiTwotoneHeart } from 'react-icons/ai';
+
+// import {
+//   getCityFollowingStatus,
+//   toggleInterestedCityToUser,
+// } from '../../services/API_proyect/city.service';
+
+// const ToggleBtnFollowCity = ({ cityToFollowId }) => {
+//   const [isFavorite, setIsFavorite] = useState(false);
+
+//   useEffect(() => {
+//     const initializeFavoriteState = async () => {
+//       try {
+//         const isFav = await getCityFollowingStatus(cityToFollowId);
+
+//         if (isFav?.data?.status === 'cityRoute is in following arr') {
+//           setIsFavorite(true);
+//         } else {
+//           setIsFavorite(false);
+//         }
+//       } catch (error) {
+//         console.error('Error while checking favorite status', error);
+//       }
+//     };
+//     initializeFavoriteState();
+//   }, [cityToFollowId]);
+
+//   const handleAddToFavorites = async () => {
+//     try {
+//       const response = await toggleInterestedCityToUser(cityToFollowId);
+//       if (response?.data?.status === 'Success updating following -- Followers') {
+//         setIsFavorite(!isFavorite);
+//       }
+
+//       // setIsFavorite(!isFavorite);
+//     } catch (error) {
+//       console.error('Error while adding to favorites', error);
+//     }
+//   };
+
+//   return (
+//     <button
+//       onClick={handleAddToFavorites}
+//       className={`ToggleButtonCity ${isFavorite ? 'favorite' : ''}`}
+//     >
+//       {isFavorite ? (
+//         <AiTwotoneHeart size={25} className="favorite-icon" />
+//       ) : (
+//         <AiOutlineHeart size={25} className="favorite-icon" />
+//       )}
+//     </button>
+//   );
+// };
+
+// export default ToggleBtnFollowCity;
